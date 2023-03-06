5d886e926862dfac15579bc67b6f368577186c41
nish4nth <96581978+nish4nth@users.noreply.github.com>
2023-03-06 23:45:11 +0530

    Update sum_of_cubes.java

--- sum_of_cubes.java
+++ sum_of_cubes.java
@@ -2,11 +2,9 @@ import java.util.Scanner;
 public class sum_of_cubes {
 	public static void main(String args[]) {
 		System.out.println("Sum of cubes of n terms");
-		System.out.println("Enter the no of terms,first term and common difference: ");
+		System.out.println("Enter the no of terms: ");
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int a=sc.nextInt();
-		int d=sc.nextInt();
 		System.out.println("Formula used: ");
 		System.out.println("((n(n+1))/2)^2");
 		float sum=(n*(n+1))/2;
